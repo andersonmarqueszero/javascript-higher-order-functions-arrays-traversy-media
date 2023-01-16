72997643b8123eae8227734fc3889c3e940c2ec6
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-16 20:30:45 -0300

    forEach + filter

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const companies= [
+const companies = [
     {name: "Company One", category: "Finance", start: 1981, end: 2003},
     {name: "Company Two", category: "Retail", start: 1992, end: 2008},
     {name: "Company Three", category: "Auto", start: 1999, end: 2007},
@@ -12,8 +12,67 @@ const companies= [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
+//for(let i = 0; i < companies.length; i++) {
+//    console.log(companies[i]);
+//}
+
 // forEach
+
+//companies.forEach(function(company){
+//    console.log(company);
+//})
+
 // filter
+//Get 21 and older
+
+//This is the method without using "filter"
+
+//let canDrink = [];
+//for(let i = 0; i < ages.length; i++) {
+//    if(ages[i] >= 21) {
+//        canDrink.push(ages[i]);
+//    }
+//}
+
+
+//Below is the method used as a regular filter function
+
+//const canDrink = ages.filter(function(age) {
+//    if(age >= 21) {
+//        return true;
+//    }
+//});
+
+//This is the same function above, but reduced a single line, using arrow functions
+
+//const canDrink = ages.filter(age => age >= 21);
+
+//console.log(canDrink);
+
+//Filter retail companies
+
+//const retailCompanies = companies.filter(function() {
+//    if(company.category === 'Retail') {
+//        return true;
+//    }
+//})
+
+//Again, the same function described above, reduced to a single line using arrow functions
+//const retailCompanies = companies.filter(company => company.category === 'Retail');
+
+//console.log(retailCompanies);
+
+//Get 80s companies
+
+//const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990));
+
+//console.log(eightiesCompanies);
+
+//Get companies that lasted more than 10 years
+
+//const lastedTenYears = companies.filter(company => (company.end - company.start >= 10));
+
+//console.log(lastedTenYears);
 // map
 // sort
 // reduce
