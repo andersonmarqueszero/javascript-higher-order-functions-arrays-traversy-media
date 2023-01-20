f1ceb41f193bdbf89467759b2e223273bcb153f7
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-01-19 22:32:19 -0300

    combining methods

--- main.js
+++ main.js
@@ -148,3 +148,13 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //const totalYears = companies.reduce((total, company) => total + (company.start - company.end), 0);
 
 //console.log(totalYears)
+
+//Combining Methods
+
+const combined = ages
+    .map(age => age * 2)
+    .filter(age => age >= 40)
+    .sort((a, b) => a - b)
+    .reduce((a, b) => a + b, 0);
+
+console.log(combined);
